74a88460ca49bec538a3cdb9cdb24594c2508df6
Jorge Mojica <jorgemojica32@gmail.com>
2025-04-12 22:09:13 -0500

    CrossOrigin para Track y Genre

--- src/main/java/com/example/demo/controller/GenreController.java
+++ src/main/java/com/example/demo/controller/GenreController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.example.demo.DTO.GenreDetailDTO;
 import com.example.demo.entity.Genre;
 import com.example.demo.service.GenreService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/genre")
 public class GenreController {
--- src/main/java/com/example/demo/controller/TrackController.java
+++ src/main/java/com/example/demo/controller/TrackController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,6 +17,7 @@ import com.example.demo.DTO.TrackDTO;
 import com.example.demo.entity.Track;
 import com.example.demo.service.TrackService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/track")
 public class TrackController {
